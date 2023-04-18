74f1bd2da889fd0ada45d42a5e7ee6b0ce2f5f59
HebaSakrH <128267375+HebaSakrH@users.noreply.github.com>
2023-04-18 21:51:15 +0200

    first Steps

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/home', (req , res) => {
+    console.log('heeeyy')
+res.sendFile(__dirname + '/views/home.html');
+});
+
+
+app.get('/about', (req , res) => {
+res.sendFile(__dirname + '/views/about.html');
+ });
+
+
+ app.get('/works', (req , res) => {
+    res.sendFile(__dirname + '/views/works.html');
+     });
+    
+     app.get('/gallery', (req , res) => {
+        res.sendFile(__dirname + '/views/gallery.html');
+         });
+        
+
+app.listen(3000, () => {
+console.log('Hellooo')
+});
+
